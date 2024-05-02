42c8e5ea1fc15d7b1ce062b7ec8867e359d7daf9
jaikumar9 <Jaiagdayemawer556@gmail.com>
2024-05-02 16:16:41 +0530

    Compleate DifferentialTesting

--- DifferentialTest/test/DifferentialTest.t.sol
+++ DifferentialTest/test/DifferentialTest.t.sol
@@ -12,7 +12,7 @@ contract DifferentialTest is Test {
         string[] memory inputs = new string[](3);
         inputs[0] = "python";
         inputs[1] = "exp.py";
-         inputs[2] = uint256(int256(x)).toString(); // Assuming int256(x).toString() converts int256 to string
+        inputs[2] = uint256(int256(x)).toString(); // Assuming int256(x).toString() converts int256 to string
         bytes memory res = vm.ffi(inputs);
         // console.log(string(res));
 
@@ -31,8 +31,12 @@ contract DifferentialTest is Test {
         // Check |y0 - y1| <= 1 = 2** 64
         uint256 DELTA = 2 ** 64;
         
-         assertApproxEqAbs(uint256(int256(y0)), uint256(int256(y1)), DELTA);
-
-
+         assertApproxEqAbs(uint256(int256(y0)), uint256(int256(y1)), DELTA); //
+            // assertApproxEqAbs are always in the positive.
+            // assertApproxEqAbs function is used to assert that a 
+            // given value left is approximately equal to another value right,
+            // with a maximum allowed difference specified by maxDelta
     }
+
+    // $ FOUNDRY_FUZZ_RUNS=200 forge test --match-path test/DifferentialTest.t.sol --ffi -vvv
 }
