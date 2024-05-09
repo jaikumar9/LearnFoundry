093626781c94ffced09d75f537596906ce589465
jaikumar9 <Jaiagdayemawer556@gmail.com>
2024-05-09 11:41:54 +0530

    Compleate the Deployment of ERC20 Token

--- deployContract/script/token.s.sol
+++ deployContract/script/token.s.sol
@@ -11,16 +11,22 @@ contract TokenScript is Script {
      }
 
      function run()public {
-        vm.startBroadcast();
+
+      uint privateKey = vm.envUint("DEV_PRIVATE_KEY");
+      address account = vm.addr(privateKey);
+      console.log("Deployer Account", account);
+
+        vm.startBroadcast(privateKey);
         //Deploy erc-20 token
-          
          Token token = new Token("Catpuss", "CATP");
 
         //mint token
-
-        token.mint(msg.sender,420000000000000000000);
-        
+        token.mint(account,420000000000000000000);
         vm.stopBroadcast();
      }
     
  }
+
+ // forge script script/token.s.sol:TokenScript --rpc-url $SEPOLIA_RPC_URL --broadcast --verify -vvvv
+
+ // Deployed contract address: 0xdfAA6F65e0948340496E95A6D471D612386B336F
